94d3cbf17320153cbc81ec84656014fa8e81d11c
Elia Kuzmanova <eliamitova7@gmail.com>
2023-02-14 21:39:28 +0100

    add home view

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ app.set('view engine', "hbs");
 
 
 
-app.use("/static", express.static("public"))
+app.use(express.static("public"))
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser());
 app.use(authentication)
