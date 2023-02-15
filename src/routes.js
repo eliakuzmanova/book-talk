2e82c1b914c261fe661c845375e2c9fc54fb886b
Elia Kuzmanova <eliamitova7@gmail.com>
2023-02-15 13:24:28 +0100

    add route guards

--- src/routes.js
+++ src/routes.js
@@ -3,25 +3,25 @@ const router = require("express").Router();
 const homeController = require("../controllers/homeController")
 const authController = require("../controllers/authController")
 const bookController = require("../controllers/bookController")
-const {authMiddleware} = require("../middlewares/authMiddleware")
+const {isAuth} = require("../middlewares/authMiddleware") //<---fix name on isAuth
 
 //add middleware to the needed routes <<<<-----------------------
 
 router.get("/", homeController.getHomeView)
 router.get("/404", homeController.get404View)
 
-router.get("/create", bookController.getCreateView)
-router.post("/create", bookController.postCreate)
+router.get("/create",isAuth, bookController.getCreateView)
+router.post("/create",isAuth, bookController.postCreate)
 
 router.get("/catalog", bookController.getCatalogView)
 
 router.get("/details/:bookId", bookController.getDetailsView)
-router.get("/wish/:bookId", bookController.getWish)
+router.get("/wish/:bookId",isAuth, bookController.getWish)
 
-router.get("/delete/:bookId", bookController.getDelete)
+router.get("/delete/:bookId",isAuth, bookController.getDelete)
 
-router.get("/edit/:bookId", bookController.getEditView)
-router.post("/edit/:bookId", bookController.postEdit)
+router.get("/edit/:bookId",isAuth, bookController.getEditView)
+router.post("/edit/:bookId",isAuth, bookController.postEdit)
 
 router.get("/register", authController.getRegisterView);
 router.post("/register", authController.postRegister);
@@ -29,6 +29,6 @@ router.post("/register", authController.postRegister);
 router.get("/login", authController.getLoginView);
 router.post("/login", authController.postLogin);
 
-router.get("/logout", authController.getLogout);
+router.get("/logout",isAuth, authController.getLogout);
 
 module.exports = router
